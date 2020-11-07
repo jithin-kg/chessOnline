01dd282d8fdbef033a81b0f65da56a058c0ed225
jithin-kg <jithinjkg@gmail.com>
2020-11-07 06:02:03 +0530

    chess board with initial pieces position created

---
+++ Assets/Scripts/ChessMan.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessMan : MonoBehaviour
+{
+    //references game objects
+    GameObject controller;
+    GameObject validMovesIdication; // indicates valid moves in chess board while playing
+
+    //positions
+    private int xOnBoard = -1;
+    private int yOnBoard = -1;
+
+    private string player;
+
+    public Sprite black_rook, black_knight, black_bishop, black_king, black_queen, black_pawn;
+    public Sprite white_rook, white_knight, white_bishop, white_king, white_queen, white_pawn;
+    // Start is called before the first frame update
+
+    public void activate()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+        setCords(); //adjust the transform, setting our own co-ordinates
+        switch (this.name)
+        {
+            case "black_rook":
+                this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
+            case "black_knight":
+                this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
+            case "black_bishop":
+                this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
+            case "black_king":
+                this.GetComponent<SpriteRenderer>().sprite = black_king; break;
+            case "black_queen":
+                this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
+            case "black_pawn":
+                this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
+
+            case "white_rook":
+                this.GetComponent<SpriteRenderer>().sprite = white_rook; break;
+            case "white_knight":
+                this.GetComponent<SpriteRenderer>().sprite = white_knight; break;
+            case "white_bishop":
+                this.GetComponent<SpriteRenderer>().sprite = white_bishop; break;
+            case "white_king":
+                this.GetComponent<SpriteRenderer>().sprite = white_king; break;
+            case "white_queen":
+                this.GetComponent<SpriteRenderer>().sprite = white_queen; break;
+            case "white_pawn":
+                this.GetComponent<SpriteRenderer>().sprite = white_pawn; break;
+
+        }
+    }
+
+    public void setCords()
+    {
+        float x = xOnBoard;
+        float y = yOnBoard;
+
+        x *= 0.66f;
+        y *= 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+
+        this.transform.position = new Vector3(x, y, -1.0f);
+
+    }
+
+
+
+    public int getXBoard()
+    {
+        return xOnBoard;
+    }
+
+    public int getYBoard()
+    {
+        return yOnBoard;
+    }
+
+    public void setXOnBoard(int x)
+    {
+        xOnBoard = x;
+    }
+    public void setYOnBoard(int y)
+    {
+        yOnBoard = y;
+    }
+}
+
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject ChessPiece;
+
+    //positions for chess pieces
+    public GameObject[,] positions = new GameObject[8, 8];
+    public GameObject[] blackPieces = new GameObject[16];
+    public GameObject[] whitePieces = new GameObject[16];
+
+    private string CurrentPlayer = "white";
+    private bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //generated = Instantiate(olakka, new Vector3(0, 0, -1), Quaternion.identity);
+        //s generated.transform.Translate()
+        initBoard();
+
+    }
+    private void initBoard()
+    {
+        whitePieces = new GameObject[]
+       {
+            create("white_rook", 0, 0), create("white_knight", 1, 0),
+            create("white_bishop", 2, 0), create("white_king", 3, 0),
+            create("white_queen", 4, 0), create("white_bishop", 5, 0),
+            create("white_knight", 6, 0), create("white_rook", 7, 0),
+
+            create("white_pawn", 0, 1), create("white_pawn", 1, 1),
+            create("white_pawn",2, 1), create("white_pawn", 3, 1),
+            create("white_pawn", 4, 1), create("white_pawn", 5, 1),
+            create("white_pawn", 6, 1), create("white_pawn", 7, 1),
+
+       };
+
+        blackPieces = new GameObject[]
+        {
+            create("black_rook", 0, 7), create("black_knight", 1, 7),
+            create("black_bishop", 2, 7), create("black_king", 3, 7),
+            create("black_queen", 4, 7), create("black_bishop", 5, 7),
+            create("black_knight", 6, 7), create("black_rook", 7, 7),
+
+            create("black_pawn", 0, 6), create("black_pawn", 1, 6),
+            create("black_pawn", 2, 6), create("black_pawn", 3, 6),
+            create("black_pawn", 4, 6), create("black_pawn", 5, 6),
+            create("black_pawn", 6, 6), create("black_pawn", 7, 6),
+
+        };
+
+        //intial chess board position setup
+        for (int i = 0; i < blackPieces.Length; i++)
+        {
+            setPosition(blackPieces[i]);
+            setPosition(whitePieces[i]);
+        }
+    }
+    private GameObject create(string name, int x, int y)
+    {
+        GameObject ob = Instantiate(ChessPiece, new Vector3(0, 0, -1), Quaternion.identity);
+        ChessMan cm = ob.GetComponent<ChessMan>();
+
+        //setting name and X and Y position of a piece in board, initially
+        cm.name = name;
+        cm.setXOnBoard(x);
+        cm.setYOnBoard(y);
+        cm.activate();
+        return ob;
+    }
+    private void setPosition(GameObject obj)
+    {
+        ChessMan cm = obj.GetComponent<ChessMan>();
+        positions[cm.getXBoard(), cm.getYBoard()] = obj;
+    }
+
+    public void setPositionEmpty(int x, int y)
+    {
+        positions[x, y] = null;
+
+    }
+    public GameObject getObjectAtPosition(int x, int y)
+    {
+        return positions[x, y];
+    }
+    public bool positionOnBoard(int x, int y)
+    {
+        if(x < 0 || y < 0 || x >= positions.GetLength(0) || y >= positions.GetLength(1))
+        {
+            return false;
+        }
+        return true;
+    }
+}
